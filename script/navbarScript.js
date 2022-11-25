188b57498f40be5e8231559f9752c497a58c7cb0
hossamalihassan <hossamy2014@gmail.com>
2022-11-25 16:40:22 +0200

    restyling profile page

--- script/navbarScript.js
+++ script/navbarScript.js
@@ -1,17 +1,26 @@
 let clapboard = document.querySelector("nav .clapboard button")
 let icon = document.querySelector("nav .clapboard .icon")
 let navbarLinks = document.querySelector("nav .links")
-let clapboardClass = "fa-solid fa-clapperboard icon"
-let closeClass = "fa-solid fa-xmark icon"
+
+let clapboardClass = ""
+let closeClass = ""
+let clapboardClassFadeIn = "fa-solid fa-clapperboard icon animate__animated animate__bounceIn"
+let clapboardClassFadeOut = "fa-solid fa-clapperboard icon animate__animated animate__bounceOut"
+let closeClassFadeIn = "fa-solid fa-xmark icon animate__animated animate__bounceIn"
+let closeClassFadeOut = "fa-solid fa-xmark icon animate__animated animate__bounceOut"
 
 function toggleNavBar() {
     let visiblity = navbarLinks.getAttribute("data-visible")
 
     if(visiblity === "false"){
+        clapboardClass = clapboardClassFadeOut
+        closeClass = closeClassFadeIn
         navbarLinks.setAttribute("data-visible", true)
         navbarLinks.setAttribute("aria-expanded", true)
         changeIcon()
     } else {
+        clapboardClass = clapboardClassFadeIn
+        closeClass = closeClassFadeOut
         navbarLinks.setAttribute("data-visible", false)
         navbarLinks.setAttribute("aria-expanded", false)
         changeIcon()
@@ -19,10 +28,13 @@ function toggleNavBar() {
 }
 
 function changeIcon(){
+    console.log(clapboardClass + " " + closeClass)
     let expanded = navbarLinks.getAttribute("aria-expanded")
     if(expanded === "false"){
         icon.className = clapboardClass
+        clapboardClass = ""
     } else {
         icon.className = closeClass
+        closeClass = ""
     }
 }
