08ca1d041107a4f826bf7d6b7ed0e55cf9a8be87
hossamalihassan <hossamy2014@gmail.com>
2022-11-25 19:00:33 +0200

    fixing some bugs

--- script/navbarScript.js
+++ script/navbarScript.js
@@ -2,25 +2,17 @@ let clapboard = document.querySelector("nav .clapboard button")
 let icon = document.querySelector("nav .clapboard .icon")
 let navbarLinks = document.querySelector("nav .links")
 
-let clapboardClass = ""
-let closeClass = ""
-let clapboardClassFadeIn = "fa-solid fa-clapperboard icon animate__animated animate__bounceIn"
-let clapboardClassFadeOut = "fa-solid fa-clapperboard icon animate__animated animate__bounceOut"
-let closeClassFadeIn = "fa-solid fa-xmark icon animate__animated animate__bounceIn"
-let closeClassFadeOut = "fa-solid fa-xmark icon animate__animated animate__bounceOut"
+let clapboardClass = "fa-solid fa-clapperboard icon"
+let closeClass = "fa-solid fa-xmark icon"
 
 function toggleNavBar() {
     let visiblity = navbarLinks.getAttribute("data-visible")
 
     if(visiblity === "false"){
-        clapboardClass = clapboardClassFadeOut
-        closeClass = closeClassFadeIn
         navbarLinks.setAttribute("data-visible", true)
         navbarLinks.setAttribute("aria-expanded", true)
         changeIcon()
     } else {
-        clapboardClass = clapboardClassFadeIn
-        closeClass = closeClassFadeOut
         navbarLinks.setAttribute("data-visible", false)
         navbarLinks.setAttribute("aria-expanded", false)
         changeIcon()
@@ -28,13 +20,10 @@ function toggleNavBar() {
 }
 
 function changeIcon(){
-    console.log(clapboardClass + " " + closeClass)
     let expanded = navbarLinks.getAttribute("aria-expanded")
     if(expanded === "false"){
         icon.className = clapboardClass
-        clapboardClass = ""
     } else {
         icon.className = closeClass
-        closeClass = ""
     }
 }
